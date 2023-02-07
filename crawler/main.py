86e66c1aac14fac970f08d01ed9ccc66fa1220b3
as1999 <asaber.78.a.s@gmail.com>
2023-02-07 23:53:23 +0330

    chore: able the 'main.py' file to be start point of crawler

--- crawler/main.py
+++ crawler/main.py
@@ -1,23 +1,7 @@
-from rich.progress import Progress
-from source.tools.json_tools import appender
-from source.scraper import Scraper
-
-TARGET_URL = r'https://www.delino.com/restaurant/malibu'
+from os import path
+import crawler
 
 if __name__ == '__main__':
-    with Progress() as progress:
-        pageNumber = 1
-        lastPagecNumber = 1
-        while pageNumber <= lastPagecNumber:
-            data = []
-            fidilioScraper = Scraper()
-            scrapeTask = progress.add_task("[cyan]page:{} scrapeing...".format(pageNumber), total=1)
-            for item in fidilioScraper.start([TARGET_URL], progress, scrapeTask):
-                data.append(item)
-            storageTask = progress.add_task("[green]page:{} storaging...".format(pageNumber), total=len(data))
-            while not progress.finished:
-                appender(data, 'restaurants.json')
-                progress.update(storageTask, advance=len(data))
-            pageNumber += 1
-
-        
+    file_path = path.dirname(__file__)
+    crawler.CrawlerTool('firefox', file_path)
+    #crawler.CrawlerTool('chrome', file_path)
